248fcf08a7a6af29e78443ef26e1bf09b158f70a
Jonas van Nijnatten <jonasvannijnatten@gmail.com>
2018-12-20 12:42:19 +0100

    Update EEGLoadData.m
    - fixed general EEGLoadData.m function

--- Functions/GeneralFunctions/EEGLoadData.m
+++ Functions/GeneralFunctions/EEGLoadData.m
@@ -1,7 +1,7 @@
-function data = EEGLoadData(mainDir)
-cd([mainDir filesep 'Data']);
+function [filename, data] = EEGLoadData(handles)
+cd(handles.dir.data);
 [filename, pathname] = uigetfile({'*.mat';},'Select a 2D array');
-cd(mainDir);
+cd(handles.dir.main);
 if any(filename)
     load([pathname filename]);
 else
