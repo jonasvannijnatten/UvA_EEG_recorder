5ce178572c6c8359a2616dbd4c4ad5e3c4825306
Jonas van Nijnatten <jonasvannijnatten@gmail.com>
2018-12-20 15:22:39 +0100

    Update EEGLoadData.m
    - changed output to filename & data instead of just the data.
    - when the function is called and the loading is cancled, an empty filename is returned.

--- Functions/GeneralFunctions/EEGLoadData.m
+++ Functions/GeneralFunctions/EEGLoadData.m
@@ -1,4 +1,4 @@
-function data = EEGLoadData(mainDir)
+function [filename, data] = EEGLoadData(mainDir)
 cd([mainDir filesep 'Data']);
 [filename, pathname] = uigetfile({'*.mat';},'Select a 2D array');
 cd(mainDir);
