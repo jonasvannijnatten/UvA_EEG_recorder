9e0c303f36aa957adee67f8c4e3e744a236be4b8
Jonas van Nijnatten <jonasvannijnatten@gmail.com>
2022-09-19 19:12:49 +0200

    First submit
    First attempt at transitioning the code to the new data format

---
+++ new_data_format/import_data.m
@@ -0,0 +1,96 @@
+function data = import_data(old_data)
+% this function takes a plain matrix and converts it to the new Fieldtrip
+% like format by running the user through a few questions
+
+% check if the input data is a plain matrix
+if ~ismatrix(old_data)
+    warndlg('The selected file does not contain a simple matrix and therefore can not be imported', 'Wrong data type')
+   return
+end
+
+
+dlgtitle = 'Provide information on your data';
+dlgpromt = { ...
+    'what is the sampling rate of the data?'; ...
+    'What are the names of the channels?'; ...
+    'what do the rows represent?'; ...
+    'What do the columns represent?' ...
+    };
+
+formats = struct('type', {}, 'style', {}, 'items', {}, 'format', {}, 'limits', {}, 'size', {});
+
+formats(1,1).type   = 'edit';
+formats(1,1).format = 'integer';
+formats(1,1).style  = 'edit';
+formats(1,1).size   = [100 20];
+
+formats(2,1).type   = 'edit';
+formats(2,1).format = 'text';
+formats(2,1).style  = 'edit';
+formats(2,1).size   = [600 20];
+
+formats(3,1).type   = 'list';
+formats(3,1).format = 'text';
+formats(3,1).style  = 'popupmenu';
+formats(3,1).items  = {'samples', 'channels'};
+
+formats(4,1).type  = 'list';
+formats(4,1).format = 'text'; 
+formats(4,1).style  = 'popupmenu';
+formats(4,1).items  = {'samples', 'channels'};
+
+defaultAnswers = {256;'';'samples';'channels'};
+
+opts = inputsdlg(dlgpromt, dlgtitle, formats, defaultAnswers);
+
+%% input checks
+
+% check if the sampling rate is a positive integer
+if isempty(opts{1})
+    errordlg('please provide a sampling rate.')
+elseif ~isnumeric(opts{1}) || isnan(opts{1})
+    errordlg('please provide a numeric value for the sampling rate.')
+elseif opts{1} <= 0 || mod(opts{1},1)~=0
+    errordlg('please provide a positiver integer as sampling rate value.')
+else
+    data.fsample = opts{1};
+end
+
+
+% check if the data dimensions are different
+if strcmp(opts{3}{:}, opts{4}{:})
+    warndlg('The dimensions of the data can not be the same.')
+    return
+elseif strcmp(opts{3}{:}, 'samples') && strcmp(opts{4}{:},'channels')
+    % use the data as it is
+    data.trial = {old_data'};
+elseif strcmp(opts{3}{:}, 'channels') && strcmp(opts{4}{:},'samples')
+    % transpose the data to match the new format
+    data.trial = {old_data};
+else
+    % this should not be able to happen
+    warndlg('Hmm something strange happened.')
+end
+nrofchannels    = size(data.trial{1},1);
+nrofsamples     = size(data.trial{1},2);
+
+% check if the channel labes are chars
+if isempty(opts{2})
+    defaultLabels = strsplit(sprintf('channel%d ', 1:nrofchannels)); 
+    defaultLabels(end) = [];
+    data.label = defaultLabels';
+elseif length(strsplit(opts{2})) < nrofchannels
+    givenLabels = strsplit(opts{2});
+    defaultLabels = strsplit(sprintf('channel%d ', 1:nrofchannels)); 
+    defaultLabels(end) = [];
+    data.label = defaultLabels';
+    for ichan = 1:length(givenLabels)
+        data.label(ichan) = givenLabels(ichan);
+    end
+end
+
+data.time       = {(0:nrofsamples-1) / data.fsample};
+data.sampleinfo = [1 nrofsamples];
+
+% function end
+end
