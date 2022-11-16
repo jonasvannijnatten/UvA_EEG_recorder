9a2604e9127b945ac498415c46497506dfc0a09c
Jonas van Nijnatten <jonasvannijnatten@gmail.com>
2022-11-16 19:11:55 +0100

    Update EEGSaveData.m
    - removed handles as input argument
    - use previous filename as base, in case it is present

--- Functions/GeneralFunctions/EEGSaveData.m
+++ Functions/GeneralFunctions/EEGSaveData.m
@@ -1,26 +1,30 @@
-function [saved] = EEGSaveData(handles, data, addition)
+function [saved] = EEGSaveData(data, addition)
 
 % TO-DO: add input arg to whether to clear data after saving or not
 
 % check whether the calling function added a suggested filename addition
-if nargin == 2
+if nargin == 1
     addition = '_';    
-elseif nargin == 3
+elseif nargin == 2
     addition = ['_' addition];
 end
+
 % create new filename combining loaded file + addition
-newname = [handles.filename.String(1:end-4) addition];
+if isfield(data,'filename')
+    newname = [cd filesep 'Data' filesep data.filename.String(1:end-4) addition];
+else
+    newname = [cd filesep 'Data' filesep ];
+end
 
 % select file path and name to save
-cd(handles.dir.data);
-[filename,pathname] = uiputfile('*','Save data',newname);
-cd(handles.dir.main);
+[filename, pathname] = uiputfile({'*.*'},'Save data',newname);
 
 % if location is selected, save data
 if any(filename)
     save([pathname filename], 'data');
     fprintf('file saved: %s%s\n', pathname, filename)
     saved = 1;
+    % otherwise return 0
 else
     saved = 0;
 end
